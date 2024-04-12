f1d75e53bd0353cd11abf9f9a00d4ca00db8a9e5
Sewlz <tnam7232@gmail.com>
2024-04-12 13:27:34 +0700

    Initial commit

---
+++ Home.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+const slidesWrapper = document.querySelector(".slides-wrapper");
+const slides = document.querySelectorAll(".slider-img");
+
+function showSlide(n) {
+  slideIndex = n;
+  const offset = -(slideIndex * (slides[0].offsetWidth + 10)); // Adjust for margin-right
+  slidesWrapper.style.transform = `translateX(${offset}px)`;
+}
+
+function nextSlide() {
+  if (slideIndex < slides.length - 1) {
+    showSlide(slideIndex + 1);
+  }
+}
+
+function prevSlide() {
+  if (slideIndex > 0) {
+    showSlide(slideIndex - 1);
+  }
+}
