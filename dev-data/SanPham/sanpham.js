c0a05f645ca096d8efe514bbc2ab70503bbfbdeb
vunghiak3 <vunghiak3@gmail.com>
2024-04-14 05:56:00 +0700

    upload page sanpham and chitietsanpham and img of products

---
+++ dev-data/SanPham/sanpham.js
@@ -0,0 +1,16 @@
+const categoriesItems = document.querySelectorAll('.categories_item a')
+
+categoriesItems.forEach(item => {
+    item.addEventListener('click', event =>{
+        event.preventDefault()
+        const targetId = event.target.getAttribute('href')
+        const targetSection = document.querySelector(targetId)
+
+        if(targetSection){
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    })
+})
+
