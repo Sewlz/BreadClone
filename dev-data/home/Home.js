5f76eb91e9fd0bea3fc10db63a7745f4d2050af5
Sewlz <tnam7232@gmail.com>
2024-04-14 18:51:49 +0700

    home swiper refractor

--- dev-data/home/Home.js
+++ dev-data/home/Home.js
@@ -1,16 +1,44 @@
+//loading slide
+const images = [
+  "./../../public/imgs/sliders/BANH-MOI_THANG-11-06-1920x720.jpg",
+  "./../../public/imgs/sliders/BUFFET-LAU-BANG-CHUYEN-WEB-DESKTOP-1920x720.jpg",
+  "./../../public/imgs/sliders/CUSTARD-COVER_WEB_DESKTOP-1920x720.jpg",
+  "./../../public/imgs/sliders/HAPPY-HOUR_WEB_WEB_DESKTOP-1920x720.jpg",
+  "./../../public/imgs/sliders/NEW-BANH_WEB_DESKTOP-copy-1920x720.jpg",
+];
+
+const slides_Wrapper = document.querySelector(".slides-wrapper");
+
+images.forEach((imageUrl, index) => {
+  const img = document.createElement("img");
+  img.classList.add("slider-img");
+  img.src = imageUrl;
+  img.alt = `Image ${index + 1}`;
+  slides_Wrapper.appendChild(img);
+});
+// slide scripts
 let slideIndex = 0;
 const slidesWrapper = document.querySelector(".slides-wrapper");
 const slides = document.querySelectorAll(".slider-img");
+const slideWidth = slides[0].offsetWidth + 10;
 
 function showSlide(n) {
   slideIndex = n;
-  const offset = -(slideIndex * (slides[0].offsetWidth + 10));
+  const offset = -(slideIndex * slideWidth);
   slidesWrapper.style.transform = `translateX(${offset}px)`;
 }
 
 function nextSlide() {
   if (slideIndex < slides.length - 1) {
     showSlide(slideIndex + 1);
+  } else {
+    // If on the last slide, smoothly transition to the first slide
+    slidesWrapper.style.transition = "transform 0.5s ease-in-out";
+    showSlide(0);
+    // Reset transition after the animation finishes
+    setTimeout(() => {
+      slidesWrapper.style.transition = "";
+    }, 500); // 0.5s transition duration
   }
 }
 
@@ -53,8 +81,95 @@ document.addEventListener("DOMContentLoaded", function () {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 30,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+//product swiper loader
+const product = [
+  {
+    src: "./../../public/imgs/home/productList/dsc02401_optimized.png",
+    alt: "Buns",
+  },
+  {
+    src: "./../../public/imgs/home/productList/PUDDING-min-300x300.png",
+    alt: "Buns",
+  },
+  {
+    src: "./../../public/imgs/home/productList/Seasonal-Specials-min-300x300.png",
+    alt: "Buns",
+  },
+  {
+    src: "./../../public/imgs/home/productList/SLICES-CAKE-min-300x300.png",
+    alt: "Buns",
+  },
+  {
+    src: "./../../public/imgs/home/productList/SW-min-300x300.png",
+    alt: "Buns",
+  },
+  {
+    src: "./../../public/imgs/home/productList/TOAST-min-300x300.png",
+    alt: "Buns",
+  },
+];
+
+const swiperWrapper = document.getElementById("swiper-wrapper");
+
+product.forEach((product) => {
+  const swiperSlide = document.createElement("div");
+  swiperSlide.classList.add("swiper-slide");
+  swiperSlide.innerHTML = `
+    <img src="${product.src}" alt="${product.alt}" />
+    <h4>${product.alt}</h4>
+    `;
+  swiperWrapper.appendChild(swiperSlide);
+});
+//top product swiper loader
+const topProducts = [
+  {
+    src: "./../../public/imgs/home/productList/dsc02401_optimized.png",
+    alt: "Buns",
+    href: "",
+  },
+  {
+    src: "./../../public/imgs/home/productList/PUDDING-min-300x300.png",
+    alt: "Buns",
+    href: "",
+  },
+  {
+    src: "./../../public/imgs/home/productList/Seasonal-Specials-min-300x300.png",
+    alt: "Buns",
+    href: "",
+  },
+  {
+    src: "./../../public/imgs/home/productList/SLICES-CAKE-min-300x300.png",
+    alt: "Buns",
+    href: "",
+  },
+  {
+    src: "./../../public/imgs/home/productList/SW-min-300x300.png",
+    alt: "Buns",
+    href: "",
+  },
+  {
+    src: "./../../public/imgs/home/productList/TOAST-min-300x300.png",
+    alt: "Buns",
+    href: "",
+  },
+];
+const swiperWrapperTop = document.querySelector(
+  "div.swiper:nth-child(4) > div:nth-child(1)"
+);
+
+topProducts.forEach((topProducts) => {
+  const swiperSlide = document.createElement("div");
+  swiperSlide.classList.add("swiper-slide");
+  swiperSlide.innerHTML = `
+  <img src="${topProducts.src}" alt="${topProducts.alt}" />
+  <h4>${topProducts.alt}</h4>
+  <button class="more-btn" onclick="document.location='${topProducts.href}'" style="border: 1px solid black; color: black;">Thêm vào giỏ hàng</button>
+`;
+  swiperWrapperTop.appendChild(swiperSlide);
 });
