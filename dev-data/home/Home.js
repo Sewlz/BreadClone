51846239297f711449aa01eb798d3579d3bbc97f
Sewlz <tnam7232@gmail.com>
2024-04-14 14:49:34 +0700

    home, aboutUs, contact, recruitment page updated

--- dev-data/home/Home.js
+++ dev-data/home/Home.js
@@ -19,3 +19,42 @@ function prevSlide() {
     showSlide(slideIndex - 1);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollAnimation = document.getElementById("scrollAnimation");
+  var scrollAnimation2 = document.getElementById("scrollAnimation2");
+  var decoAnimation = document.getElementById("decoAnimation");
+  var decoAnimation2 = document.getElementById("decoAnimation2");
+  var animationTriggered = false;
+
+  window.addEventListener("scroll", function () {
+    if (!animationTriggered && isElementInViewport(scrollAnimation)) {
+      scrollAnimation.style.animation = "scrollAnimation 1s ease forwards";
+      scrollAnimation2.style.animation = "scrollAnimation2 1s ease forwards";
+      animationTriggered = true;
+    }
+    if (isElementInViewport(decoAnimation)) {
+      decoAnimation.style.animation = "decoAnimation 1s ease forwards";
+      decoAnimation2.style.animation = "decoAnimation2 1s ease forwards";
+    }
+  });
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
