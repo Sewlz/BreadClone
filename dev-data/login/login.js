2a6ff47131acefc37dd4a24503b676dfe70364fc
vunghiak3 <vunghiak3@gmail.com>
2024-05-13 15:08:57 +0700

    update page login and add link login to page register of element sign in

---
+++ dev-data/login/login.js
@@ -0,0 +1,53 @@
+const showPass = document.querySelector(".fa-eye-slash");
+const hidePass = document.querySelector(".fa-eye");
+const inputPass = document.querySelector("#password");
+const inputUser = document.querySelector("#username");
+const alertUser = document.querySelector(".username-alert");
+const alertPass = document.querySelector(".password-alert");
+
+showPass.addEventListener("click", () => {
+  hidePass.style = "display: inline-block;";
+  showPass.style = "display: none;";
+  inputPass.type = "text";
+});
+hidePass.addEventListener("click", () => {
+  showPass.style = "display: inline-block;";
+  hidePass.style = "display: none;";
+  inputPass.type = "password";
+});
+
+inputUser.addEventListener("focus", () => {
+  alertUser.innerHTML =
+    '<i style="color:#2d80cb; padding:5px; font-size:small;" class="fa-solid fa-check"></i> <span>Enter mobile number or email</span>';
+});
+
+inputUser.addEventListener("blur", function () {
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const regexPhoneNumber = /^\d{10,}$/;
+  const value = inputUser.value.trim();
+
+  if (regexEmail.test(value) || regexPhoneNumber.test(value)) {
+    alertUser.innerHTML =
+      '<i style="color:green; padding:5px; font-size:small;" class="fa-solid fa-check"></i> <span>Enter mobile number or email</span>';
+    inputUser.style = "border: 2px solid green;";
+  } else {
+    alertUser.innerHTML =
+      '<i style="color:#2d80cb; padding:5px; font-size:small;" class="fa-solid fa-exclamation"></i> <span>Enter mobile number or email</span>';
+    inputUser.style = "border: 2px solid red;";
+  }
+});
+
+
+inputPass.addEventListener('focus', ()=>{
+    alertPass.innerHTML = '<i style="color:#2d80cb; padding:5px; font-size:small;" class="fa-solid fa-check"></i> <span>Enter password</span>'
+})
+inputPass.addEventListener('blur', ()=>{
+    const value = inputPass.value.trim()
+    if(value === ''){
+        alertPass.innerHTML = '<i style="color:#2d80cb; padding:5px; font-size:small;" class="fa-solid fa-exclamation"></i> <span>Enter password</span>'
+        inputPass.style = "border: 2px solid red;";
+    }else{
+        alertPass.innerHTML = '<i style="color:green; padding:5px; font-size:small;" class="fa-solid fa-check"></i> <span>Enter password</span>'
+        inputPass.style = "border: 2px solid green;";
+    }
+})
