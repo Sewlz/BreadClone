16fc99c3ed6788ec968a59d4138aab523acf735d
Sewlz <tnam7232@gmail.com>
2024-05-28 06:14:00 +0700

    rework add to cart func smh

--- dev-data/cart/cart.js
+++ dev-data/cart/cart.js
@@ -37,84 +37,75 @@ btnPlus.forEach((item, index) => {
 //     chooseLocation.classList.toggle('active-form-location')
 // })
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../../data/Product-data/product.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const products = data.data["mushroom cheese sausage"];
-      const posArrString = sessionStorage.getItem("numCart");
-
-      if (posArrString !== null && posArrString !== "") {
-        const posArr = posArrString.split(",").map(Number);
-        const tbody = document.querySelector(".tb-cart");
-        let index = 0;
-        posArr.forEach((item) => {
-          const product = products[item];
-          const trow = document.createElement("tr");
-          trow.classList.add("cart-item");
-          trow.setAttribute("index", index);
-          trow.innerHTML = `
-          <td class="product-img">
-                      <a href="../ChiTietSanPham/chitietsanpham.html">
-                        <img
-                          src="${product.img}"
-                          alssst=""
-                        />
-                      </a>
-                    </td>
-                    <td class="product-name" data-title="Sản phẩm:">
-                      <a href="../ChiTietSanPham/chitietsanpham.html"
-                        >${product.name}</a
-                      >
-                    </td>
-                    <td class="product-price" data-title="Giá:">
-                      <p>34,000</p>
-                    </td>
-                    <td class="product-quantity" data-title="Số lượng:">
-                      <div class="btn-fun-quanity">
-                        <button class="btn-minus button-number" type="button">
-                          <i class="fa-solid fa-minus"></i>
-                        </button>
-                        <input
-                          class="quantity"
-                          type="number"
-                          value="1"
-                          step="1"
-                          min="1"
-                          name="quantity-001"
-                          title="SL"
-                          pattern="[0-9]*"
-                          inputmode="numeric"
-                        />
-                        <button class="btn-plus button-number" type="button">
-                          <i class="fa-solid fa-plus"></i>
-                        </button>
-                      </div>
-                    </td>
-                    <td class="product-subtotal" data-title="Tạm tính:">
-                      <p>${product.price}</p>
-                    </td>
-                    <td class="product-remove">
-                      <a href="#remove-item-001" class="remove-item-cart"">
-                        <i class="fa-solid fa-trash"></i>
-                      </a>
-                    </td>
-          `;
-          tbody.insertBefore(trow, tbody.firstChild);
-          index++;
-        });
-      } else {
-        console.log("numCart is either null or empty.");
-      }
-      removeItem();
+  var cartArray = sessionStorage.getItem("cartItems");
+  if (cartArray !== null && cartArray !== "") {
+    let index = 0;
+    cartArray = JSON.parse(cartArray);
+    const tbody = document.querySelector(".tb-cart");
+    cartArray.forEach((item) => {
+      const trow = document.createElement("tr");
+      trow.classList.add("cart-item");
+      trow.setAttribute("index", index);
+      trow.innerHTML = `
+        <td class="product-img">
+                    <a href="../ChiTietSanPham/chitietsanpham.html">
+                      <img
+                        src="${item.imgSrc}"
+                        alssst=""
+                      />
+                    </a>
+                  </td>
+                  <td class="product-name" data-title="Sản phẩm:">
+                    <a href="../ChiTietSanPham/chitietsanpham.html"
+                      >${item.productName}</a
+                    >
+                  </td>
+                  <td class="product-price" data-title="Giá:">
+                    <p>34,000</p>
+                  </td>
+                  <td class="product-quantity" data-title="Số lượng:">
+                    <div class="btn-fun-quanity">
+                      <button class="btn-minus button-number" type="button">
+                        <i class="fa-solid fa-minus"></i>
+                      </button>
+                      <input
+                        class="quantity"
+                        type="number"
+                        value="1"
+                        step="1"
+                        min="1"
+                        name="quantity-001"
+                        title="SL"
+                        pattern="[0-9]*"
+                        inputmode="numeric"
+                      />
+                      <button class="btn-plus button-number" type="button">
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                    </div>
+                  </td>
+                  <td class="product-subtotal" data-title="Tạm tính:">
+                    <p>${item.price}</p>
+                  </td>
+                  <td class="product-remove">
+                    <a href="#remove-item-001" class="remove-item-cart"">
+                      <i class="fa-solid fa-trash"></i>
+                    </a>
+                  </td>
+        `;
+      tbody.insertBefore(trow, tbody.firstChild);
+      index++;
     });
+  } else {
+    console.log("numCart is either null or empty.");
+  }
+  removeItem();
 });
 
 function removeItem() {
   const removeButtons = document.querySelectorAll(".remove-item-cart");
   removeButtons.forEach((button) => {
     button.addEventListener("click", function () {
-      // Get the index attribute of the parent tr element
       const index = parseInt(
         button.closest(".cart-item").getAttribute("index")
       );
@@ -124,12 +115,12 @@ function removeItem() {
 }
 
 function removeCartItem(index) {
-  const posArrString = sessionStorage.getItem("numCart");
-  if (posArrString !== null && posArrString !== "") {
-    let posArr = posArrString.split(",").map(Number);
-    posArr.splice(index, 1);
-    sessionStorage.setItem("numCart", posArr.join(","));
-    console.log(posArr);
+  var cartArray = sessionStorage.getItem("cartItems");
+  if (cartArray !== null && cartArray !== "") {
+    cartArray = JSON.parse(cartArray);
+    cartArray.splice(index, 1);
+    sessionStorage.setItem("cartItems", JSON.stringify(cartArray));
+    console.log(cartArray);
     location.reload();
   } else {
     console.log("numCart is either null or empty.");
--- dev-data/menu/menu.js
+++ dev-data/menu/menu.js
@@ -189,33 +189,124 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function addToCart() {
   event.preventDefault();
+
   const itemCell = document.querySelectorAll(".cell");
-  let indexArr = sessionStorage.getItem("numCart")
-    ? sessionStorage.getItem("numCart").split(",")
+  let cartItems = sessionStorage.getItem("cartItems")
+    ? JSON.parse(sessionStorage.getItem("cartItems"))
     : [];
 
   itemCell.forEach((item) => {
-    item.addEventListener("click", () => {
-      let posIndex = item.getAttribute("pos-index");
-      indexArr.push(posIndex);
-      sessionStorage.setItem("numCart", indexArr.join(","));
+    item.querySelector(".button-product a").addEventListener("click", (e) => {
+      e.preventDefault();
+
+      var cartObj = convertItemToJson(item);
+      cartItems.push(cartObj);
+
+      sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
       updateCart();
     });
   });
+
+  console.log(cartItems);
+}
+function convertItemToJson(item) {
+  // Select the elements
+  const imgElement = item.querySelector(".img-product img");
+  const nameElement = item.querySelector(".name-product");
+  const priceElement = item.querySelector(".price");
+
+  // Extract values
+  const imgSrc = imgElement.src;
+  const productName = nameElement.textContent.trim();
+  const price = priceElement.textContent.trim();
+
+  // Create JSON object
+  const productInfo = {
+    imgSrc: imgSrc,
+    productName: productName,
+    price: price,
+  };
+
+  return productInfo;
 }
 document.addEventListener("DOMContentLoaded", function () {
   updateCart();
 });
 function updateCart() {
   const numCart = document.querySelector(".num-item-cart");
-  const numCartValue = sessionStorage.getItem("numCart");
+  const numCartValue = sessionStorage.getItem("cartItems");
 
   if (numCartValue !== null) {
-    const cartArray = numCartValue.split(",");
+    const cartArray = JSON.parse(numCartValue);
     let cartSize = cartArray.length;
     numCart.innerText = cartSize;
   } else if (numCartValue === null) {
     numCart.innerText = 0;
   }
-  console.log(sessionStorage.getItem("numCart"));
+  console.log(sessionStorage.getItem("cartItems"));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var cartArray = sessionStorage.getItem("cartItems");
+  if (cartArray !== null && cartArray !== "") {
+    let index = 0;
+    cartArray = JSON.parse(cartArray);
+    const ulbody = document.querySelector(".list-cart-menu");
+    cartArray.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("item-cart-menu");
+      listItem.setAttribute("index", index);
+      listItem.innerHTML = `
+      <div class="img-cart-menu">
+      <a href="../ChiTietSanPham/chitietsanpham.html">
+        <img src="${item.imgSrc}" alt="MUSHROOM CHEESE SAUSAGE">
+      </a>
+    </div>
+    <div class="info-cart-menu">
+      <h6 class="name-item-cart-menu">
+        <a href="../ChiTietSanPham/chitietsanpham.html" class="uppercase">
+          ${item.productName}
+        </a>
+      </h6>
+      <div class="price-quantity">
+        <span class="price-item-cart-menu">1</span>
+        x
+        <span class="quantity-item-cart-menu">${item.price}</span>
+      </div>
+      <a href="#remove-item-001" class="delete-item-cart-menu">
+        <i class="fa-sharp fa-solid fa-xmark"></i>
+      </a>
+    </div>
+        `;
+      ulbody.insertBefore(listItem, ulbody.firstChild);
+      index++;
+    });
+  } else {
+    console.log("numCart is either null or empty.");
+  }
+  removeHoverItem();
+});
+function removeHoverItem() {
+  const removeButtons = document.querySelectorAll(".delete-item-cart-menu");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = parseInt(
+        button.closest(".item-cart-menu").getAttribute("index")
+      );
+      removeCartHoverItem(index);
+    });
+  });
+}
+
+function removeCartHoverItem(index) {
+  var cartArray = sessionStorage.getItem("cartItems");
+  if (cartArray !== null && cartArray !== "") {
+    cartArray = JSON.parse(cartArray);
+    cartArray.splice(index, 1);
+    sessionStorage.setItem("cartItems", JSON.stringify(cartArray));
+    console.log(cartArray);
+    location.reload();
+  } else {
+    console.log("numCart is either null or empty.");
+  }
 }
