70b81d359a9a14c7eaada6086be10d51d95c4539
Sewlz <tnam7232@gmail.com>
2024-06-02 09:52:48 +0700

    add some swiper breakpoint to storeLoc.js

--- dev-data/storeLocation/storeLocation.js
+++ dev-data/storeLocation/storeLocation.js
@@ -68,22 +68,30 @@ function initializeSwiper() {
   swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
       768: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      500: {
         slidesPerView: 1,
         slidesPerGroup: 1,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
+      },
+      700: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      900: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
       },
       1200: {
         slidesPerView: 4,
         slidesPerGroup: 4,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
       },
     },
     pagination: {
